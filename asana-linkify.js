b6edc70081070880e798d5a400e9a58f88a1c84d
james <the.trystero@gmail.com>
2013-08-16 12:43:20 -0400

    replace asana ids with links on DOM modification and when the DOM is ready

--- asana-linkify.js
+++ asana-linkify.js
@@ -1,13 +1,62 @@
-function replaceInCommitTitle() {
-	'use strict';
-	console.log("called");
-	var commitTitle = document.querySelector('.commit-title');
-	commitTitle.innerHTML = commitTitle.innerHTML.replace(/#(\d+)/, "<a href='https://app.asana.com/0/0/$1'>#$1</a>");
-}
+(function () {
 
-document.onreadystatechange = function () {
-	'use strict';
-	if (document.readyState === "complete") {
-		replaceInCommitTitle();
-	}
-};
+	var reReplacedTicketId = /(?:">)#(\d+)(?:<\/a>)/gm;
+	var reTicketId = /#(\d+)/gm;
+	var template = "<a href=\"https://app.asana.com/0/0/$1\">#$1</a>";
+	var commitMessageSelector = '.full-commit';
+
+	var observeNodeWithHandler = function (node, handler) {
+		if (node && handler) {
+
+			// configuration of the observer:
+			var config = { attributes: true, childList: true, characterData: true };
+
+			var observer = new MutationObserver(function(mutations) {
+		  		mutations.forEach(function(mutation) {
+		  			handler(node);
+		  		});    
+			});
+
+			// pass in the target node, as well as the observer options
+			observer.observe(node, config);
+
+			var _unload = document.onunload;
+			document.onunload = function () {
+				observer.disconnect();		
+				return document.onunload.apply(document, arguments);
+			}
+
+		}
+	};
+
+	var replaceAsanaIdsWithLink = function (container) {
+		// get matching NodeList
+		var targetNodes = container.querySelectorAll(commitMessageSelector);
+
+		
+		// https://developer.mozilla.org/en-US/docs/Web/API/NodeList
+		// NodeList is not iterable via a Array.prototype.forEach, so standard for loop instead
+		for (var i = 0; i < targetNodes.length; i++) {
+			var node = targetNodes[i];
+			var hasId = reTicketId.test(node && node.innerHTML);
+			// make sure we haven't already replaced the id with a link
+			var hasReplacedId = reReplacedTicketId.test(node && node.innerHTML);
+
+			if (hasId && !hasReplacedId) {
+				node.innerHTML = node.innerHTML.replace(reTicketId, template);
+			}
+		}
+		
+	};
+
+	document.onreadystatechange = function () {
+		'use strict';
+		if (document.readyState === "complete") {
+			var container = document.querySelector('#js-repo-pjax-container');
+			replaceAsanaIdsWithLink(container);
+			observeNodeWithHandler(container, replaceAsanaIdsWithLink);
+		}
+
+	};
+
+}());
